aba449acf75fbb77d244c7978215d298981ec660
jitesh124201 <54475599+jitesh124201@users.noreply.github.com>
2022-10-05 12:56:55 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+        RWonly obj = new RWonly();
+        obj.setName("jitesh");
+        System.out.println(obj.getName());
+    }
 }
