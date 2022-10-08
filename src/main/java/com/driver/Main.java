c8905ec8c8f4fa52bf8f0b73e832f777dd949458
jitesh124201 <54475599+jitesh124201@users.noreply.github.com>
2022-10-08 09:44:34 +0530

    Encapsulation code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,8 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-        RWonly obj = new RWonly();
-        obj.setName("jitesh");
-        System.out.println(obj.getName());
+        RWOnly obj = new RWOnly();
+        obj.setName("Jitesh");
+      System.out.println(obj.getName());
     }
 }
